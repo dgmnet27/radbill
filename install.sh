234e971d6dec80754bb3027c4213884c8d43f8f1
dgmnet27 <156901578+dgmnet27@users.noreply.github.com>
2026-02-06 17:11:55 +0700

    Update BASE_URL to point to the correct repository

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Detect Architecture
 ARCH=$(dpkg --print-architecture)
-BASE_URL="https://github.com/dgmnet27/radbill/releases/latest/download"
+BASE_URL="https://github.com/radbill/radbill/releases/latest/download"
 
 echo "Detected Architecture: $ARCH"
 
